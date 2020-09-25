e8fbb4e5715283559e3d6fc890897e2c18a441e9
Wilstan Enock <49714835+wilsenock220@users.noreply.github.com>
2020-09-25 20:17:22 +0300

    add login template

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
+from django.contrib.auth import views as auth_views
 from App import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', app_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
     path('', include('App.urls')),
 ]
