ff9d03aea1fea02e092e37c0f7c521a0b15b2178
Wilstan Enock <49714835+wilsenock220@users.noreply.github.com>
2020-09-25 12:35:53 +0300

    add form validation

--- MyProject/App/views.py
+++ MyProject/App/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,5 +16,13 @@ def blog(request):
 
 
 def register(request):
-    form = UserCreationForm
-    return render(request, 'register.html', {'form':form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
